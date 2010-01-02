a16bf0bfe8845b8916940f1ada3854e5159ce477
Jon Niehof <jtniehof@gmail.com>
2010-01-02 10:36:20 -0500

    Added licensing and related files
     * Copyright notice in syslog-notify.c
     * GPL2 from http://www.gnu.org/licenses/gpl2.txt
     * AUTHORS list
     * INSTALL from automake 1.11
     * README and HACKING files for the project

--- src/syslog-notify.c
+++ src/syslog-notify.c
@@ -1,6 +1,21 @@
 /*Takes syslog output from a named pipe and directs it to the libnotify
  *notification system
- *Jonathan Niehof, 29 November 2009
+ *
+ *  This file is part of syslog-notify.
+ *  Copyright 2009 syslog-notify project (see file AUTHORS)
+ *
+ *  syslog-notify is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  syslog-notify is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with syslog-notify.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <config.h>
